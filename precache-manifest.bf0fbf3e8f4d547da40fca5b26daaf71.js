9666a17ce861e657aad2a1a02d9916fb58e98cc5
Wasim <waseem_wmgh@yahoo.com>
2020-05-01 22:05:34 +0200

    Updates

---
+++ precache-manifest.bf0fbf3e8f4d547da40fca5b26daaf71.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5b0ce283b274ae017f90b611027d9749",
+    "url": "/react-monsters/index.html"
+  },
+  {
+    "revision": "f430c833b0a87f50f1a9",
+    "url": "/react-monsters/static/css/main.078caf4d.chunk.css"
+  },
+  {
+    "revision": "8165f55c339324d134d5",
+    "url": "/react-monsters/static/js/2.cbc8ede3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-monsters/static/js/2.cbc8ede3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f430c833b0a87f50f1a9",
+    "url": "/react-monsters/static/js/main.94d4ed52.chunk.js"
+  },
+  {
+    "revision": "10ad7e8169dde72cf84d",
+    "url": "/react-monsters/static/js/runtime-main.97e0dfc2.js"
+  }
+]);
